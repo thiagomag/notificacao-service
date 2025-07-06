de34567f112951250d2c15aa1db7ec9193fb1b64
thiagomagdalena <thiagomagdalena@gmail.com>
2025-07-06 15:35:58 -0300

    correções

--- src/test/java/br/com/postechfiap/notificacaoservice/NotificacaoServiceApplicationTests.java
+++ src/test/java/br/com/postechfiap/notificacaoservice/NotificacaoServiceApplicationTests.java
@@ -1,13 +1,7 @@
 package br.com.postechfiap.notificacaoservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NotificacaoServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
